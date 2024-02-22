e9c305c831577588509c82540c9b0e24d1776bdb
Linda Nzeuffa <linda.nzeuffa@gmx.net>
2024-02-22 15:24:45 +0100

    cms changed six

--- src/app/Cms/page.js
+++ src/app/Cms/page.js
@@ -1,6 +1,6 @@
 "use client"
 import Head from "next/head"
-import { attributes, react as HomeContent } from './components/Content/home.md'
+import { attributes, react as HomeContent } from './components/Home/home.md'
 
 export default class Home extends Component {
   render() {
