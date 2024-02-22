e9b1b4b87d1f65053b7841b842deb9df877e2675
Linda Nzeuffa <linda.nzeuffa@gmx.net>
2024-02-22 15:05:20 +0100

    cms changed third

--- phographe-cms-project/next.config.js
+++ phographe-cms-project/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+    module.exports = {
+        webpack: (cfg) => {
+            cfg.module.rules.push(
+                {
+                    test: /\.md$/,
+                    loader: 'frontmatter-markdown-loader',
+                    options: { mode: ['react-component'] }
+                }
+            )
+            return cfg
+        }
+    }
+    
+
 
 module.exports = nextConfig
